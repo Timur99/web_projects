6891e6aca48419d9c4294c5956fbe6f34365eeda
Тимур Сайфуллин <stima99@mail.ru>
2024-04-03 12:21:01 +0400

    use --date

--- new_year_2024/static/js/scripts.js
+++ new_year_2024/static/js/scripts.js
@@ -57,6 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 var y = e.clientY;
 
                 cursor.style.left = x + "px";
-                cursor..top = y + "px";
+                cursor.style.top = y + "px";
             });
         });
