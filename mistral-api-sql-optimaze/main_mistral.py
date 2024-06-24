edd433c90d67c1e61b5be2d26bb1beaa3ed874f9
Timur <stima99@mail.ru>
2024-06-24 16:28:27 +0400

    mistral

---
+++ mistral-api-sql-optimaze/main_mistral.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from mistralai.client import MistralClient
+from mistralai.models.chat_completion import ChatMessage
+
+app = Flask(
+    __name__,
+    template_folder='templates',
+    static_folder='static',
+    static_url_path='/static',
+)
+
+@app.route('/')
+def index():
+    return render_template('mistral.html')
+
+@app.route('/process_text', methods=['POST'])
+def process_text_route():
+    # Получаем текст из запроса
+    text = request.json['text']
+    # Передаем текст в функцию process_text
+    result = process_text(text)
+    return jsonify(result=result)
+
+def process_text(text):
+    api_key = ""
+    model = "mistral-large-latest"
+
+    client = MistralClient(api_key=api_key)
+
+    chat_response = client.chat(
+        model=model,
+        messages=[ChatMessage(role="user", content=text)]
+    )
+
+    print(chat_response.choices[0].message.content)
+    return chat_response.choices[0].message.content
+
+if __name__ == '__main__':
+    app.run(debug=True)
